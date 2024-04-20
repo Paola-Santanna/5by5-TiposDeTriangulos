43d239f955f592dd0882a06cb2ca7530a1dca6c6
Paola-Santanna <paolinhasj@gmail.com>
2024-04-20 14:36:29 -0300

    #Tipo de Triângulo Foi adiocinado a estrutura de decisão, para classificar o tipo de triângulo e as estruturas de repetição para o usuário digitar o valor certo.

--- Tipos de Triângulos/Program.cs
+++ Tipos de Triângulos/Program.cs
@@ -5,13 +5,42 @@ Console.WriteLine("--- Tipos de Triâgulos ---");
 float lado_1, lado_2, lado_3;
 
 //Entrada de dados e operações
-Console.WriteLine("\nInsira os valores para cada lado do triângulo: ");
-Console.Write("Lado 1: ");
-lado_1 = float.Parse(Console.ReadLine());
-Console.Write("Lado 2: ");
-lado_2 = float.Parse(Console.ReadLine());
-Console.Write("Lado 3: ");
-lado_3 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("\nInsira os valores positivos para cada lado do triângulo: ");
+
+do
+{
+    Console.Write("Lado 1: ");
+    lado_1 = float.Parse(Console.ReadLine());
+} while (lado_1 < 0);
+
+do
+{
+    Console.Write("Lado 2: ");
+    lado_2 = float.Parse(Console.ReadLine());
+} while (lado_2 < 0);
+
+do
+{
+    Console.Write("Lado 2: ");
+    lado_3 = float.Parse(Console.ReadLine());
+} while (lado_3 < 0);
+
+if (lado_1 == lado_2 || lado_1 == lado_3 || lado_2 == lado_3)
+{
+    Console.WriteLine("Esse é um triângulo ISÓCELES, pois possui dois lados com medidas iguais.");
+}
+else
+{
+    if (lado_1 == lado_2 && lado_1 == lado_3 && lado_2 == lado_3)
+    {
+        Console.WriteLine("Esse é um triâgulo EQUILÁTERO, pois possui todos os lados com medidas iguais");
+    }
+    else
+    {
+        Console.WriteLine("\nEsse é um triângulo ESCALENO, pois possui lados diferentes.");
+    }
+}
 
 //Final
 Console.WriteLine("\nPressione ENTER para encerrar...");
