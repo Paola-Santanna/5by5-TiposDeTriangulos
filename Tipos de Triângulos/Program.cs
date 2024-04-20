8a9f44e50fd1667224eaeb1097de79e74b34674a
Paola-Santanna <paolinhasj@gmail.com>
2024-04-20 14:40:15 -0300

    #Correções de Saída Foram adicionadas quebras de linha, para melhorar o entendimento do usuário.

--- Tipos de Triângulos/Program.cs
+++ Tipos de Triângulos/Program.cs
@@ -6,7 +6,7 @@ float lado_1, lado_2, lado_3;
 
 //Entrada de dados e operações
 
-Console.WriteLine("\nInsira os valores positivos para cada lado do triângulo: ");
+Console.WriteLine("\nInsira valores positivos para cada lado do triângulo: ");
 
 do
 {
@@ -28,13 +28,13 @@ do
 
 if (lado_1 == lado_2 || lado_1 == lado_3 || lado_2 == lado_3)
 {
-    Console.WriteLine("Esse é um triângulo ISÓCELES, pois possui dois lados com medidas iguais.");
+    Console.WriteLine("\nEsse é um triângulo ISÓCELES, pois possui dois lados com medidas iguais.");
 }
 else
 {
     if (lado_1 == lado_2 && lado_1 == lado_3 && lado_2 == lado_3)
     {
-        Console.WriteLine("Esse é um triâgulo EQUILÁTERO, pois possui todos os lados com medidas iguais");
+        Console.WriteLine("\nEsse é um triâgulo EQUILÁTERO, pois possui todos os lados com medidas iguais");
     }
     else
     {
