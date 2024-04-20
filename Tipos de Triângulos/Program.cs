b62f36f5559e00079c66455d8b921e91da68752d
Paola-Santanna <paolinhasj@gmail.com>
2024-04-20 14:12:57 -0300

    Foram adionadas as variáveis e a entrada de dados.

--- Tipos de Triângulos/Program.cs
+++ Tipos de Triângulos/Program.cs
@@ -1,4 +1,18 @@
 ﻿//Início
+Console.WriteLine("--- Tipos de Triâgulos ---");
+
 //Declaração de variáveis
+float lado_1, lado_2, lado_3;
+
 //Entrada de dados e operações
+Console.WriteLine("\nInsira os valores para cada lado do triângulo: ");
+Console.Write("Lado 1: ");
+lado_1 = float.Parse(Console.ReadLine());
+Console.Write("Lado 2: ");
+lado_2 = float.Parse(Console.ReadLine());
+Console.Write("Lado 3: ");
+lado_3 = float.Parse(Console.ReadLine());
+
 //Final
+Console.WriteLine("\nPressione ENTER para encerrar...");
+Console.ReadKey();
